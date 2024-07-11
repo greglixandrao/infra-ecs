58c343ddd711b4428580f210d97cda550b7db51f
Greg Lixandrão <contato@greglixandrao.com>
2024-07-10 22:41:11 -0300

    fix VPC file

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -11,41 +11,43 @@ module "vpc" {
   enable_nat_gateway = false
   enable_vpn_gateway = false
 
-#   tags = {
-#     environment = "prod"
-#   }
+  #   tags = {
+  #     environment = "prod"
+  #   }
 }
 
 # Create a VPC endpoint for ECR dkr in the private subnet
-resource "aws_vpc_endpoint_service" "ecr_dkr_endpoint" {
-  depends_on  = [aws_ecr_repository.ecr_repository]
-  service_name = "com.amazonaws.${var.aws_region}.ecr.dkr"
-  id = module.vpc.vpc_id
-  service_type = "Interface"
-#   private_dns_enabled = true
-  acceptance_required = false
+resource "aws_vpc_endpoint" "ecr_dkr_endpoint" {
+#   depends_on          = [aws_ecr_repository.ecr_repository]
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_id              = module.vpc.vpc_id
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  #   acceptance_required = false
 
-  subnet_ids = module.vpc.private_subnets
-  security_group_ids = [aws_security_group.interface_endpoints.id, aws_security_group.privatenet.id]
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [aws_security_group.vpc_endpoint_service.id]
 }
 
 # Create a VPC endpoint for ECR API in the private subnet
-resource "aws_vpc_endpoint_service" "ecr_api_endpoint" {
-  depends_on  = [aws_ecr_repository.ecr_repository]
-  service_name = "com.amazonaws.${var.aws_region}.ecr.api"
-  id = module.vpc.vpc_id
-  service_type = "Interface"
-#   private_dns_enabled = true
-  acceptance_required = false
+resource "aws_vpc_endpoint" "ecr_api_endpoint" {
+#   depends_on          = [aws_ecr_repository.ecr_repository]
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_id              = module.vpc.vpc_id
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  #   acceptance_required = false
 
-  subnet_ids = module.vpc.private_subnets
-  security_group_ids = [aws_security_group.interface_endpoints.id, aws_security_group.privatenet.id]
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [aws_security_group.vpc_endpoint_service.id]
 }
 
 # VPC Gateway endpoint - S3
-resource "aws_vpc_endpoint_service" "s3" {
-  depends_on  = [aws_ecr_repository.ecr_repository]
-  id       = module.vpc.vpc_id
+resource "aws_vpc_endpoint" "s3" {
+#   depends_on   = [aws_ecr_repository.ecr_repository]
+  vpc_id       = module.vpc.vpc_id
   service_name = "com.amazonaws.${var.aws_region}.s3"
-  acceptance_required = false
+  #   acceptance_required = false
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = module.vpc.public_route_table_ids
 }
